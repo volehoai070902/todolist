0fc902ecfb1c47aaddb374e8079aeb12ab818cce
caubexudua <volehoai070902@gmail.com>
2024-02-09 15:02:21 +0700

    [FIX]-Fix env

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,10 @@ import 'package:todo_list/state/providers/is_loading_provider.dart';
 import 'package:todo_list/views/login/login_view.dart';
 import 'package:todo_list/views/main/main_view.dart';
 import 'firebase_options.dart';
-
+import 'package:flutter_config/flutter_config.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await FlutterConfig.loadEnvVariables();
   await Firebase.initializeApp(
   options: DefaultFirebaseOptions.currentPlatform,);
   runApp(
